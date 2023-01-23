2ea81254c005d9d82f787e94347389f828ed06e5
YUVA 45 <NIKITA@yuva.com>
2023-01-23 17:10:27 +0530

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>hello</h1>
-      <p>git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/nikita2patel/codehide.git
-git push -u origin main</p>
+   
     </div>
   )
 }
