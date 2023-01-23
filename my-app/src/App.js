ea4b2b57b839fd078a5712621f5f7b9cba2c2c75
YUVA 45 <NIKITA@yuva.com>
2023-01-23 15:31:21 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nikita2patel/codehide.git
+git push -u origin main</p>
+    </div>
+  )
+}
+
+export default App
